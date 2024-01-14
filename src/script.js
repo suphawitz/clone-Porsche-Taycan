7ade4fca6243ea5a4d940e5c3db9140f11167643
Aum Suphawit Jaikaewma <suphawit.aum.si@gmail.com>
2024-01-15 01:57:22 +0700

    model&data, highlight

--- src/script.js
+++ src/script.js
@@ -22,6 +22,8 @@ window.onscroll = () => {
 
 // Slider in SlickJs
 $(document).ready(function(){
+
+    // Header info slider
     $('.model-slider').slick({
         dots: true,
         infinite: false,
@@ -30,11 +32,15 @@ $(document).ready(function(){
         slidesToScroll: 1,
     });
 
+    // Model & Data slider
     $('.model-data-slider').slick({
-        centerMode: true,
+        dots: true,
         infinite: false,
-        centerPadding: '60px',
-        slidesToShow: 3,
+        // centerPadding: '-60px',
+        slidesToScroll: 1,
+        slidesToShow: 2,
+        // centerMode: true,
+        variableWidth: true,
         responsive: [
             {
                 breakpoint: 768,
@@ -57,6 +63,67 @@ $(document).ready(function(){
         ]
     });
 
-    $('.model-slider .slick-prev').html('<i class="fa-solid fa-angle-left"></i>');
-    $('.model-slider .slick-next').html('<i class="fa-solid fa-angle-right"></i>');
+    // Highlight slider
+    $('.highlight-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: false,
+        adaptiveHeight: true,
+        infinite: false,
+        useTransform: true,
+        speed: 400,
+        cssEase: 'cubic-bezier(0.77, 0, 0.18, 1)',
+    });
+   
+    $('.highlight-info')
+        .on('init', function(event, slick) {
+            $('.highlight-info .slick-slide.slick-current').addClass('is-active');
+        })
+        .slick({
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            dots: true,
+            focusOnSelect: false,
+            infinite: false,
+            centerMode: false,
+            focusOnSelect: true,
+            responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            }, {
+                breakpoint: 720,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+               }
+            }, {
+                breakpoint: 520,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+           }
+            }]
+        });
+   
+    $('.highlight-slider').on('afterChange', function(event, slick, currentSlide) {
+        $('.highlight-info').slick('slickGoTo', currentSlide);
+        var currrentNavSlideElem = '.highlight-info .slick-slide[data-slick-index="' + currentSlide + '"]';
+        $('.highlight-info .slick-slide.is-active').removeClass('is-active');
+        $(currrentNavSlideElem).addClass('is-active');
+    });
+   
+    $('.highlight-info').on('click', '.slick-slide', function(event) {
+        event.preventDefault();
+        var goToSingleSlide = $(this).data('slick-index');
+   
+        $('.highlight-slider').slick('slickGoTo', goToSingleSlide);
+    });
+
+    $('.slider .slick-prev').html('<i class="fa-solid fa-angle-left"></i>');
+    $('.slider .slick-next').html('<i class="fa-solid fa-angle-right"></i>');
+
 });
